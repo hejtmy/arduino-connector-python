fe5da2a1544278e9056a00be146f5416cc9a6c93
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-03-13 20:09:46 +0100

    Adds main arduino class

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,37 +1,65 @@
 /*
- Name:		Sketch1.ino
- Created:	3/13/2016 6:43:19 PM
- Author:	hejtmy
+Name:		Sketch1.ino
+Created:	3/13/2016 6:43:19 PM
+Author:	hejtmy
 */
 String serialInput;
+
 int timeout = 25;
+bool connected = false;
+//speed for the delay factor
+int speed = 20;
+
+enum Orders {BLINK, WRITE};
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  Serial.begin(9600); 
-  Serial.setTimeout(timeout);
-    while (!Serial) {
-      ; // wait for serial port to connect. Needed for native USB
-    }
+	Serial.begin(9600);
+	Serial.setTimeout(timeout);
+	while (!Serial) {
+		; // wait for serial port to connect. Needed for native USB
+	}
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-    serialInput = Serial.readString();
-    if (serialInput == "WHO\n"){
-      lettingKnow();
-    }
-    delay(1);
+	while (!connected) {
+		
+		serialInput = Serial.readStringUntil('\n');
+		if (serialInput == "WHO") {
+			lettingKnow();
+		}
+		delay(speed);
+	}
+	while (true) {
+		ListenForOrders();
+	}
 }
 
-void lettingKnow(){
-  while (true){
-    serialInput = Serial.readString();
-    if (serialInput == "DONE"){
-      break;
-    }
-    Serial.println("ARDUINO");
-    delay(1);
-  }
+void lettingKnow() {
+	while (true) {
+		serialInput = Serial.readStringUntil('\n');
+		if (serialInput == "DONE") {
+			connected = true;
+			break;
+		}
+		Serial.println("ARDUINO");
+		delay(speed);
+	}
 }
 
+void ListenForOrders() {
+	serialInput = Serial.readStringUntil('\n');
+	if(serialInput != "")
+		switch (serialInput) {
+		case 'BLINK':
+			break;
+		default:
+			break;
+		}
+	delay(speed);
+}
+
+Orders ConvertSerialReadToEnum(String str) {
+
+}

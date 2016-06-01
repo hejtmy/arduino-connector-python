2bcc6ebd3451412d9dc57eacf7943feef25607f2
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-06-01 02:48:02 +0200

    Basically completely rewrites the thing

--- Python/Arduino.py
+++ Python/Arduino.py
@@ -4,7 +4,10 @@ Created on Mon Feb  8 19:26:38 2016
 @author: Smoothie
 """
 
-import serial, time, msvcrt, sys
+import serial
+import time
+import sys
+from helpers import serial_ports
 
 class Arduino():
     def __init__(self, port="COM5", baudrate=9600, timeout=0.01):
@@ -13,67 +16,57 @@ class Arduino():
         self.arduinoConnection.timeout = timeout
         self.arduinoConnection.baudrate = baudrate
     def connect(self):
-        if (isOpen()): return (true);
-        port = self.tryPorts();
+        if (isOpen()): return (True);
         try:
-            self.arduinoConnection.open();
+            connection = self.tryPorts();
         except Exception as ex:
             print("Couldn't connect to arduino!")
             print(ex)
-            msvcrt.getch()
-            sys.exit(1)
+        if(connection):
+            self.arduinoConnection = connection
+            self.sendPrepared()
+    ### CONNECTION PART
     def tryPorts(self):
         if(self.isOpen()): self.disconnect();
         if(self.arduinoConnection.port != ""):
-            if(self.tryConnect(self.arduinoConnection.port)): return
-
+            if(self.tryConnect(self.arduinoConnection.port)): return connection
+        ports = serial_ports(10);
+        for port in ports:
+            connection = self.tryConnect(port)
+            if(connection): return connection
+        return False
+    def tryConnect(self,port):
+        connection = serial.Serial()
+        connection.port = port
+        connection.rts = True
+        connection.dtr = True
+        connection.open()
+        if(connection.is_open):
+            if(self.testConnection(connection)): return connection
+        else: return False
+    def testConnection(self,connection):
+        connection.timeout = 50;
+        connection.write(b'WHO!')
+        line = connection.readline();
+        if("ARDUINO" in line):
+            return True
+        else:
+            return False
     def isOpen(self):
         return(self.arduinoConnection.isOpen)
     def disconnect(self):
-        try:
+        if(self.isOpen()):
+            self.restart()
             self.arduinoConnection.close()
-        except Exception:
-            print("Couldn't disconnect from arduino!")
-    def flush(self):
-        self.arduinoConnection.flushInput()
-        self.arduinoConnection.flushOutput()
-    def stopSending(self):
-        if (self.arduinoConnection.isOpen() == True):
-            self.arduinoConnection.write(b"STOP")
-        else:
-            print("Arduino is not connected!")
     def reset(self):
         if (self.arduinoConnection.isOpen()):
             self.sendMessage("RESET")
-        else:
-            print("Arduino is not connected!")
-    def beep(self):
-        self.arduinoConnection.write(b"BEEP")
-    def prepare(self):
-        if self.arduinoConnection.isOpen():
-            self.arduinoConnection.read_all()
-
-            while (True):
-                while (self.arduinoConnection.inWaiting() == 0):
-                    pass
-                if (self.arduinoConnection.readline() == b"CX37"):
-                    self.stopSending()
-                    self.arduinoConnection.flushOutput()
-                    self.arduinoConnection.flushInput()
-                    break
-        else:
-            print("Arduino is not connected!")
-    def isPushed(self):
-        if (self.arduinoConnection.in_waiting > 0):
-            #            before = time.clock()
-            receivedText = self.arduinoConnection.readline()
-            #            after = time.clock()
-            #            print("Received text: ", receivedText, "Reading buffer took: %fs" %(after - before))
-            if (receivedText == b"PUSHED"):
-                return True
-            else:
-                print("Incorrect received text: ", receivedText)
-                return False
+    def blink(self):
+        self.sendMessage("BLINK")
+    def sendPrepared(self):
+        self.sendMessage("DONE")
     def sendMessage(self,message):
-        message = message +"!"
+        message = message + "!"
+        byteMessage = str.encode(message)
+        #bytearray(message,"utf-8")
         self.arduinoConnection.write(message)

2b9dc4dfb6c9894523e69f8cc33b7d00b6fdcaff
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-06-03 14:06:15 +0200

    Moved photoresistor to where it is actually useful.

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,7 +1,7 @@
 /*
-Name:		Sketch1.ino
-Created:	3/13/2016 6:43:19 PM
-Author:	hejtmy
+  Name:		Sketch1.ino
+  Created:	3/13/2016 6:43:19 PM
+  Author:	hejtmy
 */
 String serialInput;
 int timeout = 25;
@@ -24,77 +24,79 @@ char untilChar = '\!';
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	Serial.begin(9600);
-	Serial.setTimeout(timeout);
-	while (!Serial) {
-		; // wait for serial port to connect. Needed for native USB
-	}
+  Serial.begin(9600);
+  Serial.setTimeout(timeout);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	serialInput = Serial.readStringUntil(untilChar);
-	if (serialInput != "") {
-		if (serialInput == "WHO") {
-			lettingKnow();
-		}	
-		if (connected) {
-			ListenForOrders();
-		}
-		if (photoresistorUse) {
-			if (analogRead(photoresistorPin) > photoresistorThreshold) {
-				//doSth
-				//do it only once?
-			}
-		}
-	}
+  serialInput = Serial.readStringUntil(untilChar);
+  if (serialInput != "") {
+    if (serialInput == "WHO") {
+      lettingKnow();
+    }
+    if (connected) {
+      ListenForOrders();
+    }
+  }
+  if (photoresistorUse) {
+    if (analogRead(photoresistorPin) > photoresistorThreshold) {
+      //doSth
+      //do it only once?
+    }
+  }
 }
 
 void lettingKnow() {
-	float time = millis();
-	while (true) {
-		serialInput = Serial.readStringUntil(untilChar);
-		if (serialInput == "DONE") {
-			connected = true;
-			break;
-		}
-		Serial.println("ARDUINO");
-		if (millis() - time > 1000) { break; }
-		delay(speed);
-	}
+  float time = millis();
+  while (true) {
+    serialInput = Serial.readStringUntil(untilChar);
+    if (serialInput == "DONE") {
+      connected = true;
+      break;
+    }
+    Serial.println("ARDUINO");
+    if (millis() - time > 1000) {
+      break;
+    }
+    delay(speed);
+  }
 }
 
 void ListenForOrders() {
-	if (serialInput != "") {
-		if (serialInput == "RESTART") {
-			connected = false;
-			Serial.println("DONE");
-		}
-		if (serialInput == "SENDPULSE") {
-			Serial.println("DONE");
-		}
-		if (serialInput == "PULSE+") {
-			digitalWrite(13, HIGH);
-			Serial.println("DONE");
-		}
-		if (serialInput == "PULSE-") {
-			digitalWrite(13, LOW);
-			Serial.println("DONE");
-		}
-		if (serialInput == "BLINK") {
-			digitalWrite(13, HIGH);
-			delay(100);
-			digitalWrite(13, LOW);
-			Serial.println("DONE");
-		}
-		if (serialInput == "PHOTO+") {
-			photoresistorUse = true;
-			Serial.println("DONE");
-		}
-		if (serialInput == "PHOTO-") {
-			photoresistorUse = false;
-			Serial.println("DONE");
-		}
-	}
-	delay(speed);
+  if (serialInput != "") {
+    if (serialInput == "RESTART") {
+      connected = false;
+      Serial.println("DONE");
+    }
+    if (serialInput == "SENDPULSE") {
+      Serial.println("DONE");
+    }
+    if (serialInput == "PULSE+") {
+      digitalWrite(13, HIGH);
+      Serial.println("DONE");
+    }
+    if (serialInput == "PULSE-") {
+      digitalWrite(13, LOW);
+      Serial.println("DONE");
+    }
+    if (serialInput == "BLINK") {
+      digitalWrite(13, HIGH);
+      delay(100);
+      digitalWrite(13, LOW);
+      Serial.println("DONE");
+    }
+    if (serialInput == "PHOTO+") {
+      photoresistorUse = true;
+      Serial.println("DONE");
+    }
+    if (serialInput == "PHOTO-") {
+      photoresistorUse = false;
+      Serial.println("DONE");
+    }
+  }
+  delay(speed);
 }

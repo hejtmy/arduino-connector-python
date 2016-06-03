159e9551a75256193cbd9c7909583c8bc099758c
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-06-03 13:07:02 +0200

    Adds connection functionality

--- Python/WindowTesting.py
+++ Python/WindowTesting.py
@@ -5,13 +5,16 @@ arduino.connect()
 
 def onKeyPress(event):
     key = ord(event.char);
+    if key == 32:  # space
+        arduino.connect()
+        if(arduino.isOpen()): text.insert('end', "Connected")
+        else: text.insert('end', "Not connected")
     if key == 27:  # ESC
-        text.insert("Disconnecting")
+        text.insert('end', "Disconnecting")
         arduino.disconnect()
     elif key == 13:  # Enter
-        text.insert("Blinking")
+        text.insert('end', "Blinking")
         arduino.blink()
-    text.insert('end', 'You pressed %s\n' % (event.char))
 
 root = tk.Tk()
 root.geometry('300x200')
@@ -19,3 +22,5 @@ text = tk.Text(root, background='black', foreground='white', font=('Comic Sans M
 text.pack()
 root.bind('<KeyPress>', onKeyPress)
 root.mainloop()
+
+text.insert('end', 'Press enter to blink. Escaoe to disconnect. Space to connect')
